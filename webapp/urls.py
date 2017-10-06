80d8e3e1d746109362924c4e3b804dc4bf69afeb
Nik <necolema@go.olemiss.edu>
2017-10-06 13:40:40 -0500

    Edited the html code

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.home, name ='Home'),
 	url(r'^about/', views.about, name = 'About'),
+	url(r'^gallery/', views.gallery, name = 'Gallery'),
 	]
--- webapp/views.py
+++ webapp/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
 	return render(request, 'webapp/home.html')
-	
+
 def about(request):
 	return render(request, 'webapp/about.html')
+
+def gallery(request):
+	return render(request, 'webapp/gallery.html')
